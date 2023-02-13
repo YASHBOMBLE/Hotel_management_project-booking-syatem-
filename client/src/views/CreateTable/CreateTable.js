043e80b4a164196b031bea6545d956e9bb4b17ee
yashbomble <yashbomble2002@gmail.com>
2023-02-13 18:40:39 +0530

    create table page

--- client/src/views/CreateTable/CreateTable.js
+++ client/src/views/CreateTable/CreateTable.js
@@ -2,18 +2,34 @@ import React,{useEffect,useState} from 'react'
 import "./CreateTable.css"
 import axios from 'axios'
 import cheackAdmin from "./../util/cheackAdmin"
+import { NavigationType } from 'react-router-dom';
+import swal from 'sweetalert';
 function CreateTable() {
-const [tableNumber,setTableNumber] = useState();
+const [tableNumber,setTableNumber] = useState('');
 
   useEffect(()=>{
    cheackAdmin();
   },[])
+ 
 
  
+
+ async function createdTable(){
+  const response = await axios.post('/createTable',{
+    tableNumber:tableNumber
+  })
+  const result=response.data.success;
+  await swal({
+    title:"Table created Successfully"
+    ico
+  })
+ }
   
   return (
     <div>
-      <input type="number" onChange={(e)=>{EventTarget.value}} />
+       <input type='text' placeholder='Enter tableNumber' 
+                  value={Text} onChange={(e) => setTableNumber(e.target.value)} />
+                  <button onClick={createdTable}> Create Table</button>
     </div>
   )
 }
