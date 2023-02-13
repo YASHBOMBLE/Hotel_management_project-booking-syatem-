d87b0c3343732611273cb89de4eeed948d0561ce
yashbomble <yashbomble2002@gmail.com>
2023-02-13 17:33:05 +0530

    profile page update

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -16,16 +16,19 @@ function Profile() {
              <Navbar user={currentUser?.name} />
              </div>
             <div className='row'>
-                <div className='col-md-6'>
+                <div className='col-md-6 main-container'>
                   <div className='profile-container'>
                     <div className='profile-img-conatiner'>
                         <img src={user} className='user-img' />
+                        
                     </div>
+                    <hr />
                     <span className='user-info size'>Welcome  {currentUser?.name} </span>
                     <br/>
                     <span className='user-info'>Email : {currentUser?.email} </span>
                      <br/>
-                    <span className='user-info'>Phone No :{currentUser?.phone}</span>
+                    <span className='user-info'>Phone No : {currentUser?.phone}</span> <br />
+                    <span className='user-info'>Role : {currentUser?.role}</span>
                     </div>  
                 </div>
             </div>
