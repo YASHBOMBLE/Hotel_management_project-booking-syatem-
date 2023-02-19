b5cd1bf1c22d7778d82a69282f41b81b10d6fa1e
yashbomble <yashbomble2002@gmail.com>
2023-02-19 12:34:48 +0530

    feat : admin signup update

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -142,6 +142,14 @@ function Profile() {
     }
    
   
+    async function createAdmin(){
+        await swal({
+            title:"Success ",
+            icon: "success"
+        })
+
+        window.location.href = "/signupAdmin"
+    }
    
     function adminView() {
        
@@ -156,7 +164,7 @@ function Profile() {
                         <button className='btn btn-primary' onClick={createTable}>Create Table</button>
                          <button className='btn btn-primary' onClick={addFoodItem} >Add FoodItem</button>
                          <button className='btn btn-primary' onClick={fetchOrders} >View Orders</button>
-                         <button className='btn btn-primary createadminbtn'  ><Link to='/signupAdmin'>Create Admin</Link> </button>
+                         <button className='btn btn-primary createadminbtn' onClick={createAdmin} >Create Admin </button>
                         <button className='btn btn-primary' onClick={unBook} >Unbook Table</button>
                        
 
